f8f92c2e146d89a3a9afbfc5cb235731f5d90b8e
Eliel J. E. Escoto <elieljeescoto0@gmail.com>
2024-09-11 13:20:54 -0600

    REFACTOR: get user function improved

---
+++ resources/js/components/services/GetAuthUser.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import config from './Config';
+
+const GetAuthUser = async (token) => {
+	try {
+		const response = await fetch(`${config.apiBaseUrl}/user`, {
+			method: 'POST',
+			headers: {
+				'Authorization': `Bearer ${token}`,
+				'Content-Type': 'application/json',
+			},
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			return data;
+		}
+
+	} catch (error) {
+		console.error('Error fetching user data:', error)
+		return null;
+	}
+}
+
+export default GetAuthUser;
