8dc3cbda4509dddd2844d524736c3a72ea4a2b95
Jose Montes <joemontes90@gmail.com>
2024-11-16 19:48:11 -0600

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,6 @@ class AuthController extends Controller
             'maxhp' => $maxhp,
             'hp' => $hp,
             'level' => $level,
-            'exp' => $exp,
         ]);
 
         $token = auth('api')->login($user);
