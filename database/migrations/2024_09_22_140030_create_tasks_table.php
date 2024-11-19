8d0754caec9aac2ad2b2c488bd3ee5b7df2810d1
Jose Montes <joemontes90@gmail.com>
2024-11-18 19:09:10 -0600

    Task Fixed

--- database/migrations/2024_09_22_140030_create_tasks_table.php
+++ database/migrations/2024_09_22_140030_create_tasks_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('description')->nullable();
+            $table->integer('estimated_time')->default(24);
             $table->enum('difficulty', ['facil', 'medio', 'dificil']);
             $table->enum('status', ['pendiente', 'completado'])->default('pendiente');
             $table->boolean('completed')->default(false);
