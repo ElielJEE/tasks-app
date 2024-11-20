72b3681195be35665b456165d0afe0fc486fc5e4
Eliel J. E. Escoto <77648539+ElielJEE@users.noreply.github.com>
2024-11-20 14:05:56 -0600

    Vercel fixes 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="images/logo.svg" type="image/x-icon">
+    @vite(['resources/js/main.jsx', 'resources/sass/index.scss'])
     <title>CarpeDiem</title>
 </head>
 <body>
